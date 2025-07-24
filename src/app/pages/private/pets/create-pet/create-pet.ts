1a261d65b7cbab807171d8d8b40fa07e82d3c4d7
Juan D Lopez <juandiegolopezortiz4@gmail.com>
2025-07-23 21:42:49 -0500

    Se empieza la creación del formulario para crear mascotas

--- src/app/pages/private/pets/create-pet/create-pet.ts
+++ src/app/pages/private/pets/create-pet/create-pet.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { PetsService } from '../../../../services/pets-service';
+
 
 @Component({
   selector: 'app-create-pet',
-  imports: [],
+  imports: [ ReactiveFormsModule ],
   templateUrl: './create-pet.html',
   styleUrl: './create-pet.css'
 })
 export class CreatePet {
+  form!: any;
+  ngOnInit() {
+    this.form = this.fb.group( { 
+    name: ['', Validators.required],
+    type: ['', Validators.required],
+    race: ['', Validators.required],
+    age: [0, [Validators.required, Validators.min(1)]],
+    owner: ['', Validators.required]
+  })
+  }
+  
 
+  constructor( private fb: FormBuilder, private petsService: PetsService, private router: Router ) {}
+
+  submit() {
+    if ( this.form.valid ) {
+      this.petsService.createPet(this.form.value).subscribe(() => {
+        this.router.navigateByUrl( '/pets' )
+      })
+    }
+  }
 }
